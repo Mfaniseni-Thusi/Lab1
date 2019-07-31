bb085a7550b589976e2e9016946e024827125a11
Mfaniseni-Thusi <kwazithusi6@gmail.com>
2019-07-31 16:47:46 +0200

    Exercise 3.2

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -8,8 +8,10 @@ using namespace std; // cout, endl, complex are all part of this namespace
 
 int main()
 {
-	auto num1 = complex<float>{2.0, 2.0};  // use auto for type deduction
-	auto num2 = complex<float>{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
+    using complexType = complex<float>; // Used aliasing HERE
+
+	auto num1 = complexType {2.0, 2.0};  // use auto for type deduction
+	auto num2 = complexType {4.0, -2.0}; // use uniform initialisation syntax (curly braces)
 
 	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
 
