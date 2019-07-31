233163b7c4b958b25996f7485ac2f254b8240a1e
Mfaniseni-Thusi <kwazithusi6@gmail.com>
2019-07-31 18:19:18 +0200

    Exercise 4.1

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -26,6 +26,34 @@ int main()
 	myScreen.move(8,5);
 	myScreen.set("SCREEN");
 	myScreen.display();
+    cout << endl;
+
+    myScreen.clear(' ');
+	for(int row=1; row<=6; row++){
+        myScreen.move(row,1);
+        myScreen.set('*');
+	}
+
+	myScreen.move(3,2);
+	myScreen.set('*');
+    myScreen.move(3,3);
+	myScreen.set('*');
+	myScreen.move(3,4);
+	myScreen.set('*');
+	myScreen.move(2,5);
+	myScreen.set('*');
+    myScreen.move(1,6);
+	myScreen.set('*');
+	myScreen.move(4,5);
+	myScreen.set('*');
+	myScreen.move(5,6);
+	myScreen.set('*');
+	myScreen.move(6,6);
+	myScreen.set('*');
+
+	myScreen.display();
+	cout << endl;
+	myScreen.clear();
 
 	return 0;
 }
